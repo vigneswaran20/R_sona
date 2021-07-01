1f741d3a3a69b0f1d6bccde7903e3067a1f8b7d4
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-01 22:17:29 +0530

    Add files via upload

--- r_pro.R
+++ r_pro.R
@@ -144,16 +144,23 @@ tail(salem_trak)
 
 salem_trak %>%
   mutate(NEW_DATE = wday(NEW_DATE, label = TRUE)) %>%
-  mutate(CASES = fct_collapse(CASES, Fatal = c("Fatal","Fatal and accidental fire","Fatal.","Fatals"),
-                              
-                              Injury = c("Grivious Injury","Minor injury", "Minor Injury", "No injury", "Non Injury" ,"Grivious injury", "Grivious injury.","Grivious injury @ Fatal",
+  mutate(CASES = fct_collapse(CASES, Injury = c("Grivious Injury","Minor injury", "Minor Injury", "No injury", "Non Injury" ,"Grivious injury", "Grivious injury.","Grivious injury @ Fatal",
                                          "Grivious injury","Low injury","Low injury and sec 185 MV Act",
-                                         "Medium injury","Medium injury @ Low injury","Medium injury and 185 MVI Act","Medium injury`"))) %>%
+                                         "Medium injury","Medium injury @ Low injury","Medium injury and 185 MVI Act",
+                                         "Medium injury`"),  Fatal = c("Fatal","Fatal and accidental fire","Fatal.","Fatals")
+                              
+  )) %>%
   count(NEW_DATE, CASES) %>%
   group_by(CASES) %>%
   mutate(percent = n / sum(n)) %>%
   ungroup() %>%
+  filter(CASES == "Injury") %>%
   ggplot(aes(percent, NEW_DATE, fill = CASES)) +
-  geom_col(position = "dodge", alpha = 0.8) +
+  geom_col(position = "dodge", alpha = 0.8, fill="steelblue") +
   scale_x_continuous(labels = scales::percent_format()) +
-  labs(x = "% of Accidents", y = NULL, fill = "Cases")
+  labs(x = "% of Accidents", y = NULL, fill = "Cases") + ggtitle("Fatality Rate") +
+  
+  ##scale_fill_brewer(palette="Paired") +
+ 
+ theme_minimal() + theme(plot.title = element_text(hjust = 0.5),text = element_text( size = 12, family = "Open Sans"))
+
