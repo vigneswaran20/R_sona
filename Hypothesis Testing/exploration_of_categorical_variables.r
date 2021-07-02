573645e7e20368a453a62f64fb115aea6ce10ba4
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-02 19:06:40 +0530

    Update exploration_of_categorical_variables.r

--- Hypothesis Testing/exploration_of_categorical_variables.r
+++ Hypothesis Testing/exploration_of_categorical_variables.r
@@ -58,4 +58,17 @@ skimr::skim(salem_formatted)
 plot_bar(salem_formatted, ggtheme = theme_minimal(base_size = 20))
 
 '------------------------------------------------------------------------------'
+##PERCENTAGE COMPARISON
 
+library(SmartEDA)
+library(ISLR)
+
+salem_formatted
+
+ExpCatViz(salem_formatted %>% select(CASES, GENDER), target = "CASES", col=c("slateblue4","slateblue1"))
+
+'------------------------------------------------------------------------'
+##FINDING THE MEDIAN AGE
+
+plot4 <- ExpNumViz(salem_formatted,target="CASES",col=c("darkgreen","springgreen3","springgreen1"))
+plot4[[1]]
