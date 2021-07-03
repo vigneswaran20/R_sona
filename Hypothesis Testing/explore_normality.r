e4e1e1157f0c0d1b88d650416a27edf4772498dc
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-03 23:25:44 +0530

    Update explore_normality.r

--- Hypothesis Testing/explore_normality.r
+++ Hypothesis Testing/explore_normality.r
@@ -4,3 +4,4 @@
 
 'If the data is normally distributed we can use parametric test like T-TEST, ANOVA'
 'However if not normally ditributed we should use non-parametric test like Mann-Whitney or Kruskal-Wallis'
+'ANOVA and Kruskal-Wallis for more than two groups'
