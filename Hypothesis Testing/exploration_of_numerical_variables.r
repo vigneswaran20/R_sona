6d265ca660a8d696e524a4f5ab8ee76f0f4523be
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-03 21:26:38 +0530

    Update exploration_of_numerical_variables.r

--- Hypothesis Testing/exploration_of_numerical_variables.r
+++ Hypothesis Testing/exploration_of_numerical_variables.r
@@ -86,3 +86,16 @@ ExpNumStat(salem_formatted, by="GA", gp="CASES", Outlier = TRUE, Qnt = c(.25,.75
 library(summarytools)
 install.packages("summarytool")
 dfSummary(salem_formatted)
+'--------------------------------------------------------'
+'For checking categorical variable'
+
+library(gtsummary)
+install.packages("gtsummary")
+
+salem_formatted %>% select(CASES, GENDER) %>% tbl_summary(by = CASES) %>% add_p() 
+mtcars %>% select(cyl, gear, am) %>% tbl_summary(by = am)  %>% add_p() 
+'For Numeric variables it uses Non-parametric Wilcoxon test for comparing with two groups'
+'categorical variables are used in Fischers exact test if number of variables in any observations 
+is below 5 or we use CHI-SQUARE test'
+str(mtcars)
+'-------------------------------------------------------------------------------'
