2c1c9c78b06f0c5840c30368fc7b0c671f2d65c1
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-03 23:18:07 +0530

    Update exploration_of_categorical_variables.r

--- Hypothesis Testing/exploration_of_categorical_variables.r
+++ Hypothesis Testing/exploration_of_categorical_variables.r
@@ -1,50 +1,5 @@
 'Exploring categorical variables with Fischers and chi-square test'
 
-library("tidyverse")
-library("DataExplorer")
-
-
-library(readxl)
-library(lubridate)
-library(RSocrata)
-salem_accidents <- excel_sheets("E:\\CopyOFnew.xlsx") %>% map_df(~read_xlsx("E:\\CopyOFnew.xlsx",.))
-skimr::skim(salem_accidents)
-str(salem_accidents)
-
-
-'----------------------------------------------------------------------------------------------'
-
-salem_acc_report <- salem_accidents
-str(salem_acc_report)
-salem_acc_report
-
-diamonds
-skimr::skim(salem_acc_report)
-
-##ignore the missing data
-
-salem_acc <- salem_acc_report %>%
-  filter( !is.na(GENDER), !is.na(CASES), !is.na(NEW_DATE))  %>%
-  select(AGE, GENDER,CASES, NEW_DATE) %>% 
-  mutate_if(is.character, factor)
-
-
-salem_trak = salem_acc %>% arrange(desc(NEW_DATE)) %>% na.omit()
-salem_trak
-
-
-##Categorical variable exploration
-
-salem_formatted <-  salem_trak %>%
-  mutate(NEW_DATE = floor_date(NEW_DATE, unit = "week")) %>%
-  filter(
-    GENDER != "N"
-  ) %>% 
-  mutate(CASES = fct_collapse(CASES, Fatal = c("Fatal","Fatal and accidental fire","Fatal.","Fatals"),
-                              
-                              Injury = c("Grivious Injury","Minor injury", "Minor Injury", "No injury", "Non Injury" ,"Grivious injury", "Grivious injury.","Grivious injury @ Fatal",
-                                         "Grivious injury","Low injury","Low injury and sec 185 MV Act",
-                                         "Medium injury","Medium injury @ Low injury","Medium injury and 185 MVI Act","Medium injury`"))) 
 
 salem_formatted %>%  filter(
   GENDER != "N"
