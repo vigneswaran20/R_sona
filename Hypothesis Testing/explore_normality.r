55437d60555884262c3261859f94fc24fc0557ec
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-03 23:24:53 +0530

    Update explore_normality.r

--- Hypothesis Testing/explore_normality.r
+++ Hypothesis Testing/explore_normality.r
@@ -1,2 +1,6 @@
 #Yet to do
 'Explore Normality with QQ-PLOTS & SHAPIRO-WILK'
+
+
+'If the data is normally distributed we can use parametric test like T-TEST, ANOVA'
+'However if not normally ditributed we should use non-parametric test like Mann-Whitney or Kruskal-Wallis'
