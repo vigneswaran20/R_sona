a9e33268d528aa0ba75900c47dae4bc8a04adc23
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-04 03:23:36 +0530

    Update explore_normality.r

--- Hypothesis Testing/explore_normality.r
+++ Hypothesis Testing/explore_normality.r
@@ -5,3 +5,32 @@
 'If the data is normally distributed we can use parametric test like T-TEST, ANOVA'
 'However if not normally ditributed we should use non-parametric test like Mann-Whitney or Kruskal-Wallis'
 'ANOVA and Kruskal-Wallis for more than two groups'
+
+## {DataExplorer}
+library(DataExplorer)
+
+
+plot_qq(salem_formatted)
+
+plot_qq(salem_formatted, by = "CASES")
+plot_qq(salem_formatted, by = "GENDER")
+'----------------------------------------------------'
+library(dlookr)
+
+data("iris")
+
+salem_formatted %>% group_by(CASES) %>% plot_normality(AGE)
+'How close is close enough'
+
+install.packages("ggpubr")
+
+library(ggpubr)
+ggqqplot(salem_formatted,"AGE", facet.by = "CASES")
+ggqqplot(salem_formatted,"AGE", facet.by = "GENDER")
+
+
+normality(salem_formatted) %>% mutate_if(is.numeric,~round(.,3)) %>% flextable::flextable()
+
+'normality test with every numeric variable in the dataset'
+
+salem_formatted %>% group_by(CASES,GENDER) %>% normality()
