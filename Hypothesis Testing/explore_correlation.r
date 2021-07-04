ab4165315db066cc8b72cb62a9f2df9f9eabada5
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-04 16:40:07 +0530

    Update explore_correlation.r

--- Hypothesis Testing/explore_correlation.r
+++ Hypothesis Testing/explore_correlation.r
@@ -5,3 +5,5 @@ correlation(airquality, Ozone)
 
 
 plot_correlate(airquality, method = "kendall")
+ggcorrmat( data = iris)
+ggcorrmat( data = iris, type = "np" , output = "dataframe" ) %>% mutate_if(is.numeric, ~round(.,2)) %>% flextable()
