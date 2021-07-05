eb4d2441d5bd9374b5fccfd1c8f35ce113119307
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-05 08:13:50 +0530

    Create places.r

---
+++ Data Exploration/places.r
@@ -0,0 +1,23 @@
+salem_c
+View(salem_c)
+library(extrafont)
+loadfonts(device = "win")
+library(kableExtra)
+require(dplyr)
+install.packages("kableExtra")
+
+
+
+##TABLE_VIEW :-
+'------------------------'
+salem_c %>% group_by(PLACE) %>% summarise(n = max(n)) %>% arrange(desc(n)) %>%  top_n(20) %>%  mutate(scales::percent(n / sum(n))) %>%
+  kable(
+    col.names = c("PLACE", "CASES", "% of CASES"),
+    align = "llrr"
+  )
+
+
+#PLOT_VIEW :-
+'-------------------------'
+salem_c %>% group_by(PLACE) %>% summarise(n = max(n)) %>% arrange(desc(n)) %>%  top_n(8) %>% ggplot(aes(reorder(PLACE,-n), n, fill = PLACE)) +
+  geom_col(alpha = 0.8, position = "dodge", show.legend = FALSE,  width=0.4) + labs(x = NULL, y = "Case Numbers") + theme_minimal() + theme(plot.title = element_text(hjust = 0.4),text = element_text( size = 12, family = "Open Sans")) + theme(axis.text.x=element_text(angle=45, hjust=1))
