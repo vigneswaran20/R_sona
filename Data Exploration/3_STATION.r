2ba992f383f02e5dd29c040999fe0a12161de659
vigneswaran20 <86698190+vigneswaran20@users.noreply.github.com>
2021-07-05 10:21:37 +0530

    Update 3_STATION.r

--- Data Exploration/3_STATION.r
+++ Data Exploration/3_STATION.r
@@ -23,6 +23,16 @@ salem_c %>% group_by(PLACE) %>% summarise(n = max(n)) %>% arrange(desc(n)) %>%  
   geom_col(alpha = 0.8, position = "dodge", show.legend = FALSE,  width=0.4) + labs(x = NULL, y = "Case Numbers") + theme_minimal() + theme(plot.title = element_text(hjust = 0.4),text = element_text( size = 12, family = "Open Sans")) + theme(axis.text.x=element_text(angle=45, hjust=1))
 
 
+'-------------------'
+'Extract Top N Highest Values by Group Using dplyr'
+
+
+
+library("dplyr")
+salem_add_c <- salem_c %>% arrange(desc(n)) %>% group_by(NEW_DATE) %>%
+  filter(
+    NEW_DATE != 2014
+  )%>% slice(1:3) 
 
 
 
